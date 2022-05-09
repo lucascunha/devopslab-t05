5baca2db428bf9efb7ae10e0b5c7ba5392880676
Lucas Cunha <lucascunha@users.noreply.github.com>
2022-05-09 20:51:14 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Lucas Cunha - LAB DevOps & Cloud Computing"
 
 if __name__ == '__main__':
     app.run()
